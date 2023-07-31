6b1c618bbf0a7ec26599461697bd505e434f2be4
박유미 <81951313+byumm315@users.noreply.github.com>
2023-08-01 08:57:10 +0900

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -8,7 +8,8 @@ st.set_page_config(
     page_title="Home"
 )
 st.title('Population Data Clustering') #홈페이지 제목추가
-st.header('Find the features of Cluster')
+st.header('Find The Features of Cluster')
 st.image('https://img.jagranjosh.com/images/2022/July/1172022/Compress_world_Population.webp')
+st.subheader('Data samples')
 data=pd.read_csv('cluster_0801.csv')
 st.write(data.head(20))
