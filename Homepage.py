7167dd6885fdee03a5264c21ac5d373979f195f0
박유미 <81951313+byumm315@users.noreply.github.com>
2023-08-01 08:55:37 +0900

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -9,6 +9,6 @@ st.set_page_config(
 )
 st.title('Population Data Clustering') #홈페이지 제목추가
 st.header('Find the features of Cluster')
-st.image('https://www.flaticon.com/kr/free-icon/population_5075551?term=%EC%9D%B8%EA%B5%AC&page=1&position=35&origin=search&related_id=5075551')
+st.image('https://img.jagranjosh.com/images/2022/July/1172022/Compress_world_Population.webp')
 data=pd.read_csv('cluster_0801.csv')
 st.write(data.head(20))
