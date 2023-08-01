0fbd06f54a18b2a3931e0b81061a7b9610899837
박유미 <81951313+byumm315@users.noreply.github.com>
2023-08-01 10:34:44 +0900

    Update Pairplot.py

--- pages/Pairplot.py
+++ pages/Pairplot.py
@@ -25,6 +25,7 @@ data['cluster']=list(map(str,data['cluster']))
 st.subheader('Part1')
 data_1=data[v_list[0:5]]
 data_1['cluster']=data['cluster']
-fig1 = px.scatter_matrix(data_1,color='cluster',size=8)
+fig1 = px.scatter_matrix(data_1,color='cluster')
 fig1.update_traces(marker={'size':3})
+fig1.update_layout(font=dict(size=8))
 st.plotly_chart(fig1)
