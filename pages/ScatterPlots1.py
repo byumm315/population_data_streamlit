26fe0d01968917e433032b09ff37eacdeb862f9a
박유미 <81951313+byumm315@users.noreply.github.com>
2023-08-03 21:42:00 +0900

    Update ScatterPlots1.py

--- pages/ScatterPlots1.py
+++ pages/ScatterPlots1.py
@@ -7,9 +7,22 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib import font_manager
+import os
+import matplotlib.font_manager as fm
 
-plt.rcParams['font.family'] = 'NanumGothic'
-plt.rcParams['axes.unicode_minus'] = False
+@st.cache_data
+def fontRegistered():
+    font_dirs = ["C:/Users/tyumi"]
+    font_files = fm.findSystemFonts(fontpaths=font_dirs)
+
+    for font_file in font_files:
+        fm.fontManager.addfont(font_file)
+    fm._load_fontmanager(try_read_cache=False)
+    
+
+fontRegistered()
+fontNames = [f.name for f in fm.fontManager.ttflist]
+plt.rc('font', family='NanumGothic')
 
 data=pd.read_csv('cluster_0801.csv')
 
