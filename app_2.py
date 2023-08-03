3fc76a1f094d18de1310f200ce43bef38d0679a9
박유미 <81951313+byumm315@users.noreply.github.com>
2023-08-04 00:38:01 +0900

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -8,5 +8,8 @@ import numpy as np
 # 폰트 적용
 import os
 import matplotlib.font_manager as fm  # 폰트 관련 용도 as fm
-
-st.subheader(fm.findSystemFonts(fontpaths=os.getcwd()))
+font_file=fm.findSystemFonts(fontpaths=os.getcwd())
+fm.fontManager.addfont(font_file)
+fm._load_fontmanager(try_read_cache=False)
+fontNames = [f.name for f in fm.fontManager.ttflist]
+st.subheader(fontNames)
