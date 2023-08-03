76eac973195a411f9901127eb4cb5387193c435f
박유미 <81951313+byumm315@users.noreply.github.com>
2023-08-03 23:27:16 +0900

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -9,7 +9,7 @@ import numpy as np
 import os
 import matplotlib.font_manager as fm  # 폰트 관련 용도 as fm
 
-path = '/usr/share/fonts/truetype/nanum/NanumGothic_Coding.ttf' # fontlist에 있던 경로입니다
+path = '/usr/share/fonts/truetype/NanumGothic.ttf' # fontlist에 있던 경로입니다
 font_name = fm.FontProperties(fname=path).get_name()
 rc('font', family=font_name)
 st.subheader(font_name)
